27548f012245564ea9575faa5ee9d20e9b419a3a
MiirHo3eIN <amirhmoallemi@gmail.com>
2024-03-20 15:08:32 +0100

    work in progress for euclidian distance

--- vq_block.c
+++ vq_block.c
@@ -90,6 +90,60 @@ void matrix_print(float *A, char *matrix_name, int rowSize, int colSize)
 }
 
 
+void VectorToMatrixAdd(float32_t *pSrcA, float32_t* pSrcB, float32_t *pDst, int rowSizeSrcA, int rowSizeSrcB)
+{
+    int i;
+    int j;
+    
+    for (i = 0; i < rowSizeSrcA; i++)
+    {
+        for (j = 0; j < rowSizeSrcB; j++)
+        {
+            pDst[i + j] = pSrcA[i] + pSrcB[j];
+        }
+    }
+}
+
+
+void VectorInit(float32_t *pSrc, int rowSize, float value)
+{
+    int i;
+    for (i = 0; i < rowSize; i++)
+    {
+        pSrc[i] = i *value;
+    }
+}
+
+void VectorPrint(float32_t *pSrc, char *name , int rowSize)
+{
+    int i;
+    printf("Vector %s = \n", name);
+    for (i = 0; i < rowSize; i++)
+    {   
+        printf("%f\n", pSrc[i]);
+    }
+}
+
+void VectorAddTest()
+{
+    /* Init the Vectors */
+    float32_t *A;
+    float32_t *B;
+    float32_t *C;
+
+    A = (float32_t *) malloc(N_ROW * sizeof(float32_t));
+    B = (float32_t *) malloc(N_COL * sizeof(float32_t));
+    C = (float32_t *) malloc(N_ROW * N_COL *sizeof(float32_t));
+
+    VectorInit(A, N_ROW, 10);
+    VectorInit(B, N_COL, 5);
+
+    VectorToMatrixAdd(A, B, C, N_ROW, N_COL);
+
+    VectorPrint(C, "C", N_ROW*N_COL);
+
+}
+
 /* Program Entry. */
 int main(void)
 {
@@ -97,34 +151,33 @@ int main(void)
     printf("Entering main controller\n");
 
     uint32_t errors = 0;
-    const int m = 2; // Rows in A
-    const int n = 3; // Columns in A, Rows in B
-    const int p = 2; // Columns in B
     
-    float32_t *A;
-    float32_t *B;
-    float32_t *C; 
-    A = (float32_t *) malloc(N_COL*N_ROW * sizeof(float32_t));
-    B = (float32_t *) malloc(N_COL*N_ROW * sizeof(float32_t));
-    C = (float32_t *) malloc(N_ROW * N_ROW * sizeof(float32_t));
+
+    VectorAddTest();
+    // float32_t *A;
+    // float32_t *B;
+    // float32_t *C; 
+    // A = (float32_t *) malloc(N_COL*N_ROW * sizeof(float32_t));
+    // B = (float32_t *) malloc(N_COL*N_ROW * sizeof(float32_t));
+    // C = (float32_t *) malloc(N_ROW * N_ROW * sizeof(float32_t));
 
     
-    // Initialize the matrices
-    MatrixInit(A, N_ROW, N_COL);
-    MatrixInit(B, N_COL, N_ROW);
-    MatrixInit(C, N_ROW, N_ROW);
+    // // Initialize the matrices
+    // // MatrixInit(A, N_ROW, N_COL);
+    // // MatrixInit(B, N_COL, N_ROW);
+    // // MatrixInit(C, N_ROW, N_ROW);
 
-    // Print the matrices
+    // // Print the matrices
 
 
-    printf("Result of matrix A:\n");
-    matrix_print( A, "A", N_ROW, N_COL);
-    matrix_print( B, "B", N_COL, N_ROW);
+    // printf("Result of matrix A:\n");
+    // matrix_print( A, "A", N_ROW, N_COL);
+    // matrix_print( B, "B", N_COL, N_ROW);
 
-    matMul(A, B, C, N_ROW, N_COL, N_ROW);
+    // matMul(A, B, C, N_ROW, N_COL, N_ROW);
 
-    matrix_print(C, "Result", N_ROW, N_ROW);
-    // vector_sum(A, C, N_ROW, N_COL);
+    // matrix_print(C, "Result", N_ROW, N_ROW);
+    // // vector_sum(A, C, N_ROW, N_COL);
     
     // // Perform matrix multiplication
     // matrixMultiply((int*)A, (int*)B, (int*)C, m, n, p);
